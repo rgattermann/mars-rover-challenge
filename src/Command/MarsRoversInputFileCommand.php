222f3c67088ccc403ae2ab2ad1c90c8663cf9f00
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2019-01-09 14:11:30 -0200

    create app commands

---
+++ src/Command/MarsRoversInputFileCommand.php
@@ -0,0 +1,83 @@
+<?php
+namespace MarsRover\Command;
+
+use SplFileObject;
+use InvalidArgumentException;
+use MarsRover\Service\Input;
+use MarsRover\Model\{Coordinate, Plateau, Rover, RoverSetup};
+use MarsRover\Collections\RoverCollection;
+use MarsRover\Service\CommandsInputParser;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\{Question, ConfirmationQuestion};
+use MarsRover\Service\Log;
+
+class MarsRoversInputFileCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('marsrovers:import');
+        $this->setDescription('import rover instructions');
+        $this->addArgument('file', null, 'File instructions');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filename = $input->getArgument('file');
+
+        if ($this->validateFile($filename)) {
+            $file = new SplFileObject($filename, 'r');
+
+            foreach ($file as $k => $line) {
+                $line = str_replace(array("\n", "\r"), Input::CMD_SEPARATOR, $line);
+                
+                if ($file->key() == 0) {
+                    
+                    list($plateau_x, $plateau_y) = explode(Input::CMD_SEPARATOR, $line);
+                    $coordinate = new Coordinate((int)$plateau_x, (int)$plateau_y);
+                    $plateau = new Plateau($coordinate);
+
+                    $roverCollection = new RoverCollection;
+                    $squadCounter = 0;
+                    continue;
+                }
+
+                if (Input::isDigit(Input::toArray($line))) {
+                    if ($roverCollection->offsetExists($squadCounter) == false) {
+                        $rover = new Rover;
+                        $rover->setSetup(new RoverSetup($line));
+                        $roverCollection->offsetSet($squadCounter, $rover);
+                    }
+                } else {
+                    if ($roverCollection->offsetExists($squadCounter)) {
+                        $rover = $roverCollection->offsetGet($squadCounter);
+                        $rover->setCommands((new CommandsInputParser($line))->get());
+                        $squadCounter++;
+                    }
+                }
+            }
+
+            $roverCollection->executeAll();
+            $output->writeln('Commands result:');
+            $output->writeln($roverCollection->printAllSetup());
+        }
+    }
+
+    private function validateFile(string $filename): bool
+    {
+        if (empty($filename)) {
+            throw new InvalidArgumentException('File ' . $filename . ' is empty!');
+        }
+
+        if (!file_exists($filename)) {
+            throw new InvalidArgumentException('File not found: ' . $filename);
+        }
+
+        if (!is_readable($filename)) {
+            throw new InvalidArgumentException('File is not readable: ' . $filename);
+        }
+
+        return true;
+    }
+}
---
+++ src/Command/MarsRoversInputStdinCommand.php
@@ -0,0 +1,63 @@
+<?php
+namespace MarsRover\Command;
+
+use MarsRover\Service\Input;
+use MarsRover\Model\{Coordinate, Plateau, Rover, RoverSetup};
+use MarsRover\Collections\RoverCollection;
+use MarsRover\Service\CommandsInputParser;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\{Question, ConfirmationQuestion};
+
+class MarsRoversInputStdinCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('marsrovers:input');
+        $this->setDescription('Input rover instructions');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+        $plateau_dimensions = $helper->ask($input, $output, (new Question('Plateau dimensions: ', 3)));
+
+        list($plateau_x, $plateau_y) = explode(Input::CMD_SEPARATOR, $plateau_dimensions);
+        $coordinate = new Coordinate((int)$plateau_x, (int)$plateau_y);
+        $plateau = new Plateau($coordinate);
+
+        $roverCollection = new RoverCollection;
+        $squadCounter = 0;
+        $add_new_rover = true;
+        
+        while ($add_new_rover === true) {
+            $command = $helper->ask($input, $output, (new Question('Rover position: ')));
+
+            if ($roverCollection->offsetExists($squadCounter) == false) {
+                $rover = new Rover;
+                $rover->setSetup(new RoverSetup($command));
+                $roverCollection->offsetSet($squadCounter, $rover);
+            }
+
+            $command = $helper->ask($input, $output, (new Question('Rover commands: ')));
+
+            if ($roverCollection->offsetExists($squadCounter)) {
+                $rover = $roverCollection->offsetGet($squadCounter);
+                $rover->setCommands((new CommandsInputParser($command))->get());
+                $squadCounter++;
+            }
+
+            $question = new ConfirmationQuestion(
+                'Add other rover?',
+                false,
+                '/^(y|j)/i'
+            );
+            $add_new_rover = $helper->ask($input, $output, $question);
+        }
+
+        $roverCollection->executeAll();
+        $output->writeln('Commands result:');
+        $output->writeln($roverCollection->printAllSetup());
+    }
+}
